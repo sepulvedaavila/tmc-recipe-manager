07aa4877e15d69be0cbd50136e888b45b19f386f
Carlos Sepulveda <cnsa1606@gmail.com>
2025-04-07 14:24:23 -0600

    Port config

--- src/api/server.js
+++ src/api/server.js
@@ -39,7 +39,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
