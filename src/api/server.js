9cee4b7c8ebeb19074417443787c276619bad16e
Carlos Sepulveda <cnsa1606@gmail.com>
2025-04-07 18:14:51 -0600

    Fixing ports 2

--- src/api/server.js
+++ src/api/server.js
@@ -79,7 +79,7 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
